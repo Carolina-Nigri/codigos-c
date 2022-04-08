ece7fe1e60937282d463d6a8a483629609c5f8a0
Carolina-Nigri <102864941+Carolina-Nigri@users.noreply.github.com>
2022-04-08 18:13:12 -0300

    Correcao numeros_primos.c

--- AEDs_I/Lista_7/numeros_primos.c
+++ AEDs_I/Lista_7/numeros_primos.c
@@ -15,6 +15,7 @@ int main(){
         if(Erro) printf("\n\aValor invalido!\n");
     } while(Erro);
 
+/* Primeira forma (for)
     int QtdeDiv = 0;
     int Resto;
 
@@ -27,6 +28,39 @@ int main(){
 
     if(Primo) printf("\nO numero %i eh primo.\n",N);
     else printf("\nO numero %i nao eh primo.\n",N);
+*/
+
+/* Segunda forma (while)
+    int QtdeDiv = 0;
+    int Resto;
+    int Div = 2;
+
+    while((QtdeDiv < 1) && (Div <= (N/2))){
+        Resto = N % Div;
+        if(Resto == 0) QtdeDiv++;
+        Div++;
+    } // fim while
+
+    bool Primo = (N > 1) && (QtdeDiv == 0);
+
+    if(Primo) printf("\nO numero %i eh primo.\n",N);
+    else printf("\nO numero %i nao eh primo.\n",N);
+*/     
+
+///* Terceira forma (while - melhor)
+    bool Primo = true;
+    int Div = 2;
+    int Resto;
+
+    while(Primo && (Div <= (N/2))){
+        Resto = N % Div;
+        if(Resto == 0) Primo = false;
+        Div++;
+    } // fim while
+
+    if(Primo && (N > 1)) printf("\nO numero %i eh primo.\n",N);
+    else printf("\nO numero %i nao eh primo.\n",N);
+//*/  
 
     return 0;
 } // fim main()
