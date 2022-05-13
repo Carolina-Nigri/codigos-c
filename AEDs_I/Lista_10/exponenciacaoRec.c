c85c5dea392662f6ad5a6bc70b091be8ecac1a9f
Carolina-Nigri <102864941+Carolina-Nigri@users.noreply.github.com>
2022-05-13 17:25:11 -0300

    Add exponenciacaoRec.c

---
+++ AEDs_I/Lista_10/exponenciacaoRec.c
@@ -0,0 +1,29 @@
+// Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+// Protótipos
+float potencia(float Base, int Expoente);
+
+int main(){
+    printf("\n\tCalcula exponenciacao\n\n");
+
+    printf("%f\n",potencia(2,10));
+
+    return 0;
+} // fim main()
+
+float potencia(float Base, int Expoente){
+   float Potencia = 1;
+
+    if(Expoente > 0) Potencia = Base;
+    else if(Expoente < 0) Potencia = 1.0 / Base;
+
+    if(Expoente > 1){
+        Potencia = Base * potencia(Base,Expoente - 1);
+    } else if(Expoente < -1){
+        Potencia = (1.0 / Base) * potencia(Base,Expoente + 1);
+    }
+
+    return Potencia;
+} // fim potencia() 
